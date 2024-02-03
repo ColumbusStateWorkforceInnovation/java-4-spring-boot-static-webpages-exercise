2ee0f74a9d232d3ad19bd22e44758dcfcc842b24
Jim Kirkbide <jkirkbride@cscc.edu>
2024-02-03 11:15:00 -0500

    Initial commit.

---
+++ src/test/java/edu/cscc/springbootstarter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cscc.springbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
